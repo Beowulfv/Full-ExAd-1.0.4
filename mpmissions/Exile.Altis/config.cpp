9c7e15088a12191a7ec260661278d1322de624f4
Beowulfv <beowolfv@gmail.com>
2019-01-21 17:45:32 -0500

    Add files via upload

---
+++ mpmissions/Exile.Altis/config.cpp
@@ -0,0 +1,310 @@
+class CfgXM8
+{
+	class settings
+	{
+		controlID = 4070;
+		appID = "App01";
+		title = "Settings";
+	};
+	
+	class healthScanner
+	{
+		controlID = 4120;
+		appID = "App02";
+		title = "Health Scanner";
+	};
+	
+	class slothMachine
+	{
+		controlID = 4140;
+		appID = "App03";
+		title = "Sloth Machine";
+	};
+	
+	extraApps[] = {"ExAd_VG","ExAd_Info","ExAd_CHVD","ExAd_Journal","ExAd_Bike","ExAd_Quad","ExAd_SB"};
+	
+	class ExAd_VG 
+	{
+		title = "Virtual Garage";
+		controlID = 50000;					//IDC:50000 -> 50015 || These need to be unique and out of range from each other 
+		logo = "ExadClient\XM8\Apps\VG\Icon_VG.paa";
+		onLoad = "ExAdClient\XM8\Apps\VG\onLoad.sqf";
+		onOpen = "ExAdClient\XM8\Apps\VG\onOpen.sqf";
+		onClose = "ExAdClient\XM8\Apps\VG\onClose.sqf";
+	};	
+	class ExAd_Info 
+	{
+		title = "Server Info";
+		controlID = 50100;					//IDC:50100 -> 50102 || These need to be unique and out of range from each other
+		logo = "ExadClient\XM8\Apps\Info\Icon_SI.paa";
+		onLoad = "ExAdClient\XM8\Apps\Info\onLoad.sqf";
+		onOpen = "ExAdClient\XM8\Apps\Info\onOpen.sqf";
+		onClose = "ExAdClient\XM8\Apps\Info\onClose.sqf";
+	};	
+	class ExAd_CHVD 
+	{
+		title = "View Distance Settings";
+		controlID = 50200;					//IDC:50200 -> 50102 || These need to be unique and out of range from each other
+		config = "ExadClient\XM8\Apps\CHVD\config.sqf";
+		logo = "ExadClient\XM8\Apps\CHVD\Icon_CHVD.paa";
+		onLoad = "ExAdClient\XM8\Apps\CHVD\onLoad.sqf";
+		onOpen = "ExAdClient\XM8\Apps\CHVD\onOpen.sqf";
+		onClose = "ExAdClient\XM8\Apps\CHVD\onClose.sqf";
+	};		
+	class ExAd_Journal 
+	{
+		title = "Journal";
+		controlID = 50300;					//IDC:50300 -> 50305 || These need to be unique and out of range from each other
+		config = "ExadClient\XM8\Apps\Journal\config.sqf";
+		logo = "ExadClient\XM8\Apps\Journal\Icon_Journal.paa";
+		onLoad = "ExAdClient\XM8\Apps\Journal\onLoad.sqf";
+		onOpen = "ExAdClient\XM8\Apps\Journal\onOpen.sqf";
+		onClose = "ExAdClient\XM8\Apps\Journal\onClose.sqf";
+	};
+	class ExAd_Bike
+	{
+		title = "Deploy Bike";
+		config = "ExadClient\XM8\Apps\DeployVehicle\config.sqf";
+		bambiState = 0;
+		vehicleClass = "Exile_Bike_MountainBike";
+		recipe[] = {{"Exile_Item_ExtensionCord",-1}};
+		packable = 1;
+		autoCleanUp = 1;
+		quickFunction = "['ExAd_Bike'] call ExAd_XM8_DV_fnc_spawnVehicle";
+	};
+	class ExAd_Quad
+	{
+		title = "Deploy Quad";
+		bambiState = 0;
+		vehicleClass = "Exile_Bike_QuadBike_Fia";
+		recipe[] = {{"Exile_Item_ExtensionCord",1}};
+		packable = 1;
+		quickFunction = "['ExAd_Quad'] call ExAd_XM8_DV_fnc_spawnVehicle";
+	};
+	class ExAd_SB 
+	{
+		title = "Statsbar Settings";
+		controlID = 50400;					//IDC:50400 -> 50475 || These need to be unique and out of range from each other
+		logo = "ExadClient\XM8\Apps\SB_Settings\Icon_SB.paa";
+		onLoad = "ExAdClient\XM8\Apps\SB_Settings\onLoad.sqf";
+	};
+};
+
+class XM8_App01_Button: RscExileXM8AppButton1x1
+{
+    textureNoShortcut = "\exile_assets\texture\ui\xm8_app_settings_ca.paa";
+    text = "Settings";
+    onButtonClick = "['settings', 0] call ExileClient_gui_xm8_slide";
+    resource = "XM8SlideSettings";
+};
+
+class XM8_App02_Button: RscExileXM8AppButton1x1
+{
+    textureNoShortcut = "\exile_assets\texture\ui\xm8_app_health_scanner_ca.paa";
+    text = "Health Scanner";
+    onButtonClick = "['healthScanner', 0] call ExileClient_gui_xm8_slide";
+    resource = "XM8SlideHealthScanner";
+};
+
+class XM8_App03_Button: RscExileXM8AppButton1x1
+{
+    textureNoShortcut = "\exile_assets\texture\ui\xm8_app_slothMachine_ca.paa";
+    text = "Sloth Machine";
+    onButtonClick = "['slothMachine', 0] call ExileClient_gui_xm8_slide";
+    resource = "XM8SlideSlothMachine";
+};
+
+class XM8_App04_Button: RscExileXM8AppButton1x1
+{
+    textureNoShortcut = "\exile_assets\texture\ui\xm8_app_boom_ca.paa";
+    text = "BOOM!";
+    onButtonClick = "call ExileClient_system_breaching_detonate";
+    resource = "";
+};
+
+class XM8_App05_Button: RscExileXM8AppButton1x1
+{
+    textureNoShortcut = "ExadClient\XM8\Apps\VG\Icon_VG.paa";
+    text = "Virtual Garage";
+    onButtonClick = "['ExAd_VG', 0] call ExileClient_gui_xm8_slide";
+    resource = "";
+};
+
+class XM8_App06_Button: RscExileXM8AppButton1x1
+{
+    textureNoShortcut = "ExadClient\XM8\Apps\Info\Icon_SI.paa";
+    text = "Server Info";
+    onButtonClick = "['ExAd_Info', 0] call ExileClient_gui_xm8_slide";
+    resource = "";
+};
+
+class XM8_App07_Button: RscExileXM8AppButton1x1
+{
+    textureNoShortcut = "ExadClient\XM8\Apps\CHVD\Icon_CHVD.paa";
+    text = "View Distance Settings";
+    onButtonClick = "['ExAd_CHVD', 0] call ExileClient_gui_xm8_slide";
+    resource = "";
+};
+
+class XM8_App08_Button: RscExileXM8AppButton1x1
+{
+    textureNoShortcut = "ExadClient\XM8\Apps\Journal\Icon_Journal.paa";
+    text = "Journal";
+    onButtonClick = "['ExAd_Journal', 0] call ExileClient_gui_xm8_slide";
+    resource = "";
+};
+
+class XM8_App09_Button: RscExileXM8AppButton1x1
+{
+    textureNoShortcut = "\exile_assets\texture\ui\xm8_app_settings_ca.paa";
+    text = "Deploy Bike";
+    onButtonClick = "['ExAd_Bike', 0] call ExAd_XM8_DV_fnc_spawnVehicle";
+    resource = "";
+};
+
+class XM8_App10_Button: RscExileXM8AppButton1x1
+{
+    textureNoShortcut = "\exile_assets\texture\ui\xm8_app_settings_ca.paa";
+    text = "Deploy Quad";
+    onButtonClick = "['ExAd_Quad', 0] call ExAd_XM8_DV_fnc_spawnVehicle";
+    resource = "";
+};
+
+class XM8_App11_Button: RscExileXM8AppButton1x1
+{
+    textureNoShortcut = "ExadClient\XM8\Apps\SB_Settings\Icon_SB.paa";
+    text = "Statsbar Settings";
+    onButtonClick = "['ExAd_SB', 0] call ExileClient_gui_xm8_slide";
+    resource = "";
+};
+
+class XM8_App12_Button: RscExileXM8AppButton1x1
+{
+    textureNoShortcut = "";
+    text = "";
+    onButtonClick = "";
+    resource = "";
+};
+
+class XM8_App13_Button: RscExileXM8AppButton1x1
+{
+    textureNoShortcut = "";
+    text = "";
+    onButtonClick = "";
+    resource = "";
+};
+
+class XM8_App14_Button: RscExileXM8AppButton1x1
+{
+    textureNoShortcut = "";
+    text = "";
+    onButtonClick = "";
+    resource = "";
+};
+
+class CfgExileCustomCode 
+{
+	ExileServer_system_territory_database_load = "ExAdClient\VirtualGarage\CustomCode\ExileServer_system_territory_database_load.sqf";
+	ExileClient_gui_xm8_slide = "ExAdClient\XM8\CustomCode\ExileClient_gui_xm8_slide.sqf";
+	ExileClient_gui_xm8_show = "ExAdClient\XM8\CustomCode\ExileClient_gui_xm8_show.sqf";
+};
+
+class CfgInteractionMenus
+{
+	class Car 
+	{
+		targetType = 2;
+		target = "Car";
+
+		class Actions 
+		{
+			class PackDeployedVehicle: ExileAbstractAction
+			{
+				title = "Pack Vehicle";
+				condition = "call ExAd_XM8_DV_fnc_canPack";
+				action = "call ExAd_XM8_DV_fnc_pack";
+			};
+		};
+	};
+	class Bikes
+	{
+		targetType = 2;
+		target = "Bicycle";
+
+		class Actions
+		{
+			class PackDeployedVehicle: ExileAbstractAction
+			{
+				title = "Pack Bike";
+				condition = "call ExAd_XM8_DV_fnc_canPack";
+				action = "call ExAd_XM8_DV_fnc_pack";
+			};
+		};
+	};
+	class Flag
+	{
+		targetType = 2;
+		target = "Exile_Construction_Flag_Static";
+		class Actions
+		{
+			class HackVG : ExileAbstractAction
+			{
+				title = "Hack Virtual Garage";
+				condition = "call ExAd_fnc_canHackVG";
+				action = "_this spawn ExAd_fnc_startHack";
+			};
+		};
+	};
+	class Construction
+	{
+		targetType = 2;
+		target = "Exile_Construction_Abstract_Static";
+
+		class Actions 
+		{
+			class Grind : ExileAbstractAction
+			{
+				title = "Grind Lock";
+				condition = "call ExAd_fnc_canGrindLock";
+				action = "_this spawn ExAd_fnc_grindLock";
+			};
+			
+			class RestoreLock : ExileAbstractAction
+			{
+				title = "Restore Lock";
+				condition = "_object call ExAd_fnc_canRestoreLock";
+				action = "_this spawn ExAd_fnc_restoreLock";
+			};
+		};
+	};
+	class Safe
+	{
+		targetType = 2;
+		target = "Exile_Container_Safe";
+
+		class Actions 
+		{
+			class HackSafe : ExileAbstractAction
+			{
+				title = "Hack Safe";
+				condition = "call ExAd_fnc_canHackSafe";
+				action = "_this spawn ExAd_fnc_startHack";
+			};
+		};
+	};
+	class Laptop
+	{
+		targetType = 2;
+		target = "Exile_Construction_Laptop_Static";
+
+		class Actions
+		{
+			class StopHack: ExileAbstractAction
+			{
+				title = "Interupt Hack";
+				condition = "(ExileClientInteractionObject getVariable ['ExAd_HACKING_IN_PROGRESS', false])";
+				action = "_this spawn ExAd_fnc_stopHack";
+			};
+		};
+	};
+};
